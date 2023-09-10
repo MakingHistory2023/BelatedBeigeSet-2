d8c723a5f266eef7b2dd2cccc96991a4dbb5430f
MakingHistory2023 <143676019+MakingHistory2023@users.noreply.github.com>
2023-09-10 00:06:39 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class PersonalInfo {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Chris Johnson");
+    Console.WriteLine ("12/01/1987");
+    Console.WriteLine ("work 000-222-0101");
+    Console.WriteLine ("cell 000-222-0189");
+  }
+}
